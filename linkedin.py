211d6165ccdc8426c0767ac228ff3951baa8a82b
mh-salari <mh.salari@sutech.ac.ir>
2022-12-24 22:02:37 +0330

    chore: add comments and a bit of optimization

--- linkedin.py
+++ linkedin.py
@@ -27,102 +27,135 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-
+# import helper functions from other modules
 from utils.send_email import send_email
 from utils.phd_keywords import phd_keywords
 from utils.compose_email import compose_email
 from utils.customers_data import customers_data
 
-log_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "linkedin.log")
+# Set path for logging
+log_file_path = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), "linkedin.log"
+)
+# Configure logging
 log.basicConfig(
-    level=log.INFO, filename=log_path, format="%(asctime)s %(levelname)s %(message)s"
+    level=log.INFO,
+    filename=log_file_path,
+    format="%(asctime)s %(levelname)s %(message)s",
 )
 
 
 def make_driver():
+    """
+    Create and return a headless Chrome webdriver instance
+    """
+
+    # Set options for Chrome
     options = Options()
     options.add_argument("--headless")
-    options.add_argument("user-data-dir=~/.chrome_driver_session")
+    options.add_argument("user-data-dir=.chrome_driver_session")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
 
+    # Create and return Chrome webdriver instance
     driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()), options=options
     )
-
     return driver
 
 
 def login_to_linkedin(driver):
+    """
+    Log in to LinkedIn using the provided email address and password
+    """
 
+    # Get email and password from environment variables
     email = os.environ["LINKEDIN_EMAIL_ADDRESS"]
     password = os.environ["LINKEDIN_PASSWORD"]
 
+    # Load LinkedIn login page
     driver.get("https://linkedin.com/uas/login")
+    # Wait for page to load
     time.sleep(5)
+    # Check if already logged in
     if driver.current_url == "https://www.linkedin.com/feed/":
         return
-    username = driver.find_element("id", "username")
-    username.send_keys(email)
-    pword = driver.find_element("id", "password")
-    pword.send_keys(password)
+    # Find email input field and enter email address
+    email_field = driver.find_element("id", "username")
+    email_field.send_keys(email)
+    # Find password input field and enter password
+    password_field = driver.find_element("id", "password")
+    password_field.send_keys(password)
+    # Find login button and click it
     driver.find_element("xpath", "//button[@type='submit']").click()
 
 
 def extract_positions_text(page_source):
-
-    _positions = []
-    positions = []
-
+    """
+    Extract position text and links from the given LinkedIn search results page source
+    """
+    # Set to store position text and links
+    positions = set()
+    # Create BeautifulSoup object to parse HTML
     soup = BeautifulSoup(page_source.replace("<br>", "\n"), "lxml")
-
+    # Find main search results element
     search_results = soup.find("main", {"aria-label": "Search results"})
-
-    _positions += search_results.find_all(
+    # Find all position text elements
+    position_elements = search_results.find_all(
         "div",
         {"class": "update-components-text relative feed-shared-update-v2__commentary"},
     )
-    positions += [position.text.strip() for position in _positions]
-
-    links_p1 = search_results.find_all(
+    # Find all links elements
+    link_elements = search_results.find_all(
         "div",
         {
             "class": "feed-shared-update-v2 feed-shared-update-v2--minimal-padding full-height relative feed-shared-update-v2--e2e artdeco-card"
         },
     )
-
-    links_p2 = search_results.find_all(
+    link_elements += search_results.find_all(
         "div",
         {
             "class": "feed-shared-update-v2 feed-shared-update-v2--minimal-padding full-height relative artdeco-card"
         },
     )
-    for links in links_p1 + links_p2:
+    # Add position text to positions set
+    for position_element in position_elements:
+        position_text = position_element.text.strip()
+        if position_text:
+            positions.add(position_text)
+    # Add position text and links to positions set
+    for link_element in link_elements:
         try:
-            position_text = links.find(
+            # Find position text element
+            position_element = link_element.find(
                 "div",
                 {
                     "class": "update-components-text relative feed-shared-update-v2__commentary"
                 },
-            ).text.strip()
+            )
+            position_text = position_element.text.strip()
             if position_text:
-                positions[
-                    positions.index(position_text)
-                ] = f'{position_text}\nhttps://www.linkedin.com/feed/update/{links["data-urn"]}'
+                # Add position text and link to positions set
+                positions.add(
+                    f'{position_text}\nhttps://www.linkedin.com/feed/update/{link_element["data-urn"]}'
+                )
         except:
+            # Ignore errors
             pass
-    return list(set(positions))
+    # Return positions as list
+    return list(positions)
 
 
 def find_positions(driver, phd_keywords):
-
-    url = "https://www.linkedin.com/search/results/"
-    driver.get(url)
-    time.sleep(5)
-    all_positions = []
+    # Set to store all positions found
+    all_positions = set()
+    # Initialize progress bar
     pbar = tqdm(phd_keywords)
+    # Iterate through keywords
     for keyword in pbar:
+        # Initialize page number
         page = 0
+        # Set postfix for progress bar
         pbar.set_postfix(
             {
                 "Keyword": keyword,
@@ -130,13 +163,19 @@ def find_positions(driver, phd_keywords):
                 "TN of founded positions": len(all_positions),
             }
         )
+        # Construct URL with keyword
         url = f'https://www.linkedin.com/search/results/content/?datePosted=%22past-24h%22&keywords="{keyword}"&origin=FACETED_SEARCH&sid=c%3Bi&sortBy=%22date_posted%22'
+        # Load page
         driver.get(url)
+        # Extract positions from page source
         positions = extract_positions_text(driver.page_source)
+        # Number of positions found on current page
         len_founded_positions = len(positions)
-        scroll_flag = True
-        while scroll_flag:
+        # Iterate through pages
+        while True:
+            # Increment page number
             page += 1
+            # Set postfix for progress bar
             pbar.set_postfix(
                 {
                     "Keyword": keyword,
@@ -144,77 +183,113 @@ def find_positions(driver, phd_keywords):
                     "TN of founded positions": len(all_positions),
                 }
             )
+            # Scroll to bottom of page
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            # Wait for page to load
             time.sleep(10)
-            positions = extract_positions_text(driver.page_source)
-            positions = list(set(positions))
-            if len(positions) != len_founded_positions:
-                len_founded_positions = len(positions)
-            else:
-                scroll_flag = False
-        all_positions += positions
-
-    all_positions = list(set(all_positions))
+            # Extract positions from page source
+            new_positions = extract_positions_text(driver.page_source)
+            # Convert to set to eliminate duplicates
+            new_positions = set(new_positions)
+            # Check if positions on current page are the same as previous page
+            if new_positions == positions:
+                # If so, break out of loop
+                break
+            # Update positions
+            positions = new_positions
+        # Add positions to all_positions set
+        all_positions |= positions
+    # Convert set to list
+    all_positions = list(all_positions)
+    # Iterate through positions
     for position in all_positions:
+        # Strip out URL from position
         result = re.sub(
             r"https:\/\/www\.linkedin\.com\/feed\/update\/urn:li:activity:\d+",
             "",
             position,
         ).strip()
-        if result != position:
-            try:
-                all_positions.remove(result)
-            except:
-                pass
+        # If position without URL is in the list, remove it
+        if result in all_positions:
+            all_positions.remove(result)
+    # Return list of positions
     return all_positions
 
 
-def filter_positions(positions, keywords):
-    results = []
-    for position in positions:
-        if any(item.lower() in position.lower() for item in keywords):
-            results.append(position)
-    return results
+def filter_positions(all_positions, search_keywords):
+    """Filter the list of positions based on search keywords.
+
+    Args:
+        all_positions (list): list of positions
+        search_keywords (list): list of keywords to filter positions by
+
+    Returns:
+        list: list of positions that contain at least one of the search keywords
+    """
+    # initialize empty list to store matching positions
+    matching_positions = []
+
+    # loop through each position and check if it contains any of the search keywords
+    for position in all_positions:
+        if any(keyword.lower() in position.lower() for keyword in search_keywords):
+            matching_positions.append(position)
+
+    return matching_positions
 
 
-def compose_and_send_email(email_address, customers_name, positions, base_path):
+def compose_and_send_email(recipient_email, recipient_name, positions, base_path):
+    """Compose and send an email to the specified recipient with a list of positions.
 
-    email_text = compose_email(customers_name, "LinkedIn", positions, base_path)
-    send_email(email_address, "PhD Positions from LinkedIn", email_text, "html")
+    Args:
+        recipient_email (str): email address of the recipient
+        recipient_name (str): name of the recipient
+        positions (list): list of positions to include in the email
+        base_path (str): base path for any included links
+    """
+    email_content = compose_email(recipient_name, "LinkedIn", positions, base_path)
+    send_email(recipient_email, "PhD Positions from LinkedIn", email_content, "html")
 
 
-if __name__ == "__main__":
-
-    log.info(f"Searching LinkedIn for Ph.D. Positions")
-    base_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "utils")
+def main():
+    log.info("Searching LinkedIn for Ph.D. Positions")
+    # get base path for utils directory
+    utils_dir_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "utils")
 
     load_dotenv()
     print("[info]: Opening Chrome")
     driver = make_driver()
-    print("[info]: Logging in to the Linkedin 🐢...")
+    print("[info]: Logging in to LinkedIn 🐢...")
     login_to_linkedin(driver)
-    print("[info]: Searching for the Ph.D. positions in Linkedin 🐷")
-    positions = find_positions(driver, phd_keywords[:])
+    print("[info]: Searching for Ph.D. positions on LinkedIn 🐷...")
+    all_positions = find_positions(driver, phd_keywords[:])
     driver.quit()
-    print(f"[info]: Total number of positions: {len(positions)}")
-    log.info(f"Found {len(positions)} posts related to Ph.D. Positions")
-    today = datetime.today()
-    date = today - timedelta(days=1)
-    date = date.strftime("%Y-%m-%d")
+    print(f"[info]: Total number of positions: {len(all_positions)}")
+    log.info(f"Found {len(all_positions)} posts related to Ph.D. Positions")
+    # get yesterday's date
+    yesterday = datetime.today() - timedelta(days=1)
+    date_string = yesterday.strftime("%Y-%m-%d")
 
     for customer in customers_data:
-        if customer["expiration_date"] >= today:
+        if customer["expiration_date"] >= yesterday:
+            # get customer keywords and make them lowercase and remove spaces
             keywords = set(
-                [keyword.replace(" ", "") for keyword in customer["keywords"]]
-                + customer["keywords"]
+                [keyword.replace(" ", "").lower() for keyword in customer["keywords"]]
             )
-            related_positions = filter_positions(positions, keywords)
-            if related_positions:
+            # filter positions based on customer keywords
+            relevant_positions = filter_positions(all_positions, keywords)
+            if relevant_positions:
                 log.info(
-                    f"Sending email containing {len(related_positions)} positions to: {customer['name']}"
+                    f"Sending email containing {len(relevant_positions)} positions to: {customer['name']}"
                 )
-                print(f"[info]: sending email to: {customer['name']}")
+                print(f"[info]: Sending email to: {customer['name']}")
                 compose_and_send_email(
-                    customer["email"], customer["name"], related_positions, base_path
+                    customer["email"],
+                    customer["name"],
+                    relevant_positions,
+                    utils_dir_path,
                 )
                 time.sleep(10)
+
+
+if __name__ == "__main__":
+    main()
