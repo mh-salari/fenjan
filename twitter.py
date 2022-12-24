647e02c9ede2a3610a1fd166c610cffe43fd1ecb
mh-salari <mh.salari@sutech.ac.ir>
2022-12-24 22:02:49 +0330

    chore: add comments and a bit of optimization

--- twitter.py
+++ twitter.py
@@ -5,7 +5,7 @@ Created on Des 6 2022
 @author: Hue (MohammadHossein) Salari
 @email:hue.salari@gmail.com
 
-Sources: 
+Sources:
     - https://towardsdatascience.com/how-to-extract-data-from-the-twitter-api-using-python-b6fbd7129a33
 """
 
@@ -18,101 +18,208 @@ from datetime import datetime, timedelta
 
 import tweepy
 
+# import helper functions from other modules
 from utils.send_email import send_email
 from utils.compose_email import compose_email
 from utils.phd_keywords import phd_keywords
 from utils.customers_data import customers_data
 
-log_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "twitter.log")
+# set up logging to a file
+log_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "twitter.log")
 log.basicConfig(
-    level=log.INFO, filename=log_path, format="%(asctime)s %(levelname)s %(message)s"
+    level=log.INFO,
+    filename=log_file_path,
+    format="%(asctime)s %(levelname)s %(message)s",
 )
 
 
+import tweepy
+import os
+
+
 def connect_to_twitter_api():
+    """
+    Connect to the Twitter API.
+
+    Returns:
+        tweepy.API: Twitter API client.
+    """
+
+    # load environment variables from .env file
     load_dotenv()
 
+    # get API keys and access tokens from environment variables
     consumer_key = os.environ["API_KEY"]
     consumer_secret = os.environ["API_KEY_SECRET"]
     access_token = os.environ["ACCESS_TOKEN"]
     access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
 
+    # authenticate with the Twitter API
     auth = tweepy.OAuth1UserHandler(
         consumer_key, consumer_secret, access_token, access_token_secret
     )
 
+    # create a Twitter API client
     api = tweepy.API(auth)
 
     return api
 
 
-def find_positions(twitter_api, keywords, date):
+def find_positions(twitter_api, phd_keywords, date):
+    """
+    Find tweets containing keywords related to  funded PhD positions.
 
-    tweets = []
-    print("Searching for full funded phd positions in Twitter🐤")
-    for keyword in tqdm(keywords):
+    Parameters:
+        twitter_api (tweepy.API): Twitter API client.
+        phd_keywords (List[str]): List of keywords to search for in Twitter.
+        date (str): Date in the format "YYYY-MM-DD" to use as a lower bound for the the search.
+
+    Returns:
+        tweets set[tweepy.Status]: set of tweets containing the given keywords that posted after the given date.
+    """
+
+    # list to store found tweets
+    tweets = list()
+
+    print("Searching for PhD positions in Twitter🐤")
+    # iterate through PhD keywords and search for tweets containing each keyword
+    pbar = tqdm(phd_keywords)
+
+    for keyword in pbar:
+        pbar.set_postfix(
+            {
+                "Keyword": keyword,
+                "TN of founded positions": len(tweets),
+            }
+        )
+        # create query string for searching tweets containing the keyword that posted after the given date
         query = f'"{keyword}" since:{date} -filter:retweets'
-        tweets += twitter_api.search_tweets(
+        # search Twitter for tweets matching the query
+        found_tweets = twitter_api.search_tweets(
             query, tweet_mode="extended", result_type="recent", lang="en", count=100
         )
-
+        # add found tweets to the list of tweets
+        tweets += found_tweets
+        tweets = list(set(tweets))
     return tweets
 
 
 def clean_tweets(tweets):
+    """
+    Filter and format tweets.
+
+    Parameters:
+        tweets (List[tweepy.Status]): List of tweets to filter and format.
+
+    Returns:
+        List[str]: List of formatted tweets.
+    """
+
+    # list to store formatted tweets
     positions = []
-    _positions = []
+    # list to store previously seen tweet text
+    seen_positions = []
+
+    # iterate through tweets
     for tweet in set(tweets):
+        # generate URL for the tweet
         url = f"https://twitter.com/twitter/statuses/{tweet.id}"
         try:
-            description = tweet.retweeted_status.full_text
-
+            # get tweet description from retweeted status if possible
+            text = tweet.retweeted_status.full_text
         except AttributeError:
-            description = tweet.full_text
+            # get tweet description from original tweet if retweeted status is not available
+            text = tweet.full_text
 
-        if description not in _positions:
-            _positions.append(f"{description}")
+        # add tweet if its text has not been seen before
+        if text not in seen_positions:
+            # add tweet description to the list of seen descriptions
+            seen_positions.append(f"{text}")
+            # format tweet and add it to the list of formatted tweets
             positions.append(
-                f"date:{tweet.created_at.strftime('%Y-%m-%d %H:%M:%S')}<br>by:{tweet.user.name}<br><br>{description}<br>gi{url}"
+                f"date:{tweet.created_at.strftime('%Y-%m-%d %H:%M:%S')}<br>by:{tweet.user.name}<br><br>{text}<br>gi{url}"
             )
+
     return positions
 
 
 def filter_positions(positions, keywords):
+    """
+    Filter list of positions based on keywords.
+
+    Parameters:
+        positions (List[str]): List of positions to filter.
+        keywords (List[str]): List of keywords to use for filtering.
+
+    Returns:
+        List[str]: Filtered list of positions.
+    """
+
+    # list to store filtered positions
     results = []
+    # iterate through positions
     for text in positions:
+        # include the position in results if it contains any of the keywords
         if any(item.lower() in text.lower() for item in keywords):
             results.append(text)
     return results
 
 
-def compose_and_send_email(email_address, customers_name, positions, base_path):
+def compose_and_send_email(recipient_email, recipient_name, positions, base_path):
+    """
+    Compose and send email containing positions.
 
+    Parameters:
+        recipient_email (str): Email address to send the email to.
+        recipient_name (str): Customer's name to include in the email.
+        positions (List[str]): List of positions to include in the email.
+        base_path (str): Base path for the email template file.
+
+    Returns:
+        None
+    """
+    # generate email text using the email template and the given positions
     email_text = email_template = compose_email(
-        customers_name, "Twitter", positions, base_path
+        recipient_name, "Twitter", positions, base_path
     )
+    # send email with the generated text
+    send_email(recipient_email, "PhD Positions from Twitter", email_text, "html")
 
-    send_email(email_address, "PhD Positions from Twitter", email_text, "html")
 
-
-if __name__ == "__main__":
+def main():
+    # Log message
     log.info(f"Searching Twitter for Ph.D. Positions")
-    api = connect_to_twitter_api()
+
+    # Connect to Twitter API
+    twitter_api = connect_to_twitter_api()
+
+    # Set base path
     base_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "utils")
-    today = datetime.today()
-    date = today - timedelta(days=1)
-    date = date.strftime("%Y-%m-%d")
 
-    tweets = clean_tweets(find_positions(api, phd_keywords, date))
-    log.info(f"Found {len(tweets)} tweets related to Ph.D. Positions")
+    # Get date from yesterday
+    yesterday = datetime.today() - timedelta(days=1)
+    date = yesterday.strftime("%Y-%m-%d")
 
+    # Search for Ph.D. positions and clean up tweets
+    phd_positions = clean_tweets(find_positions(twitter_api, phd_keywords, date))
+
+    # Log number of tweets that have been found
+    log.info(f"Found {len(phd_positions)} tweets related to Ph.D. Positions")
+
+    # Loop through customers
     for customer in customers_data:
-        if customer["expiration_date"] >= today:
-            keywords = set(
+        # Check if customer's expiration date is today or later
+        if customer["expiration_date"] >= datetime.today():
+            # Get keywords for customer
+            customer_keywords = set(
                 [keyword.replace(" ", "") for keyword in customer["keywords"]]
                 + customer["keywords"]
             )
-            related_positions = filter_positions(tweets, keywords)
+
+            # Filter positions based on customer keywords
+            related_positions = filter_positions(phd_positions, customer_keywords)
+
+            # If there are related positions, send email to customer
             if related_positions:
                 log.info(
                     f"Sending email containing {len(related_positions)} positions to: {customer['name']}"
@@ -121,3 +228,7 @@ if __name__ == "__main__":
                 compose_and_send_email(
                     customer["email"], customer["name"], related_positions, base_path
                 )
+
+
+if __name__ == "__main__":
+    main()
