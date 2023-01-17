a56b3cd89732675b50829773e93c91a8a548aeb3
mh-salari <mh.salari@sutech.ac.ir>
2023-01-18 02:23:57 +0330

    refactor: use helper functions

--- go_crawlers/university_of_helsinki/go.mod
+++ go_crawlers/university_of_helsinki/go.mod
@@ -1,8 +1,12 @@
-module fenjan.ai-hue.ir/helsinki
+module fenjan.ai-hue.ir/university_of_helsinki
+
+replace fenjan.ai-hue.ir/tea => ../tea
 
 go 1.18
 
 require (
-	github.com/go-sql-driver/mysql v1.7.0 // indirect
-	github.com/joho/godotenv v1.4.0 // indirect
+	fenjan.ai-hue.ir/tea v0.0.0-00010101000000-000000000000
+	github.com/go-sql-driver/mysql v1.7.0
 )
+
+require github.com/joho/godotenv v1.4.0 // indirect
--- go_crawlers/university_of_helsinki/main.go
+++ go_crawlers/university_of_helsinki/main.go
@@ -7,69 +7,14 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 
+	"fenjan.ai-hue.ir/tea"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/joho/godotenv"
 )
 
-type Position struct {
-	Title         string `json:"title"`
-	URL           string `json:"url"`
-	Department    string `json:"department"`
-	Date          string `json:"date"`
-	DateTimestamp int    `json:"date_timestamp"`
-	Description   string `json:"description"`
-	Key           int    `json:"key"`
-}
+type Position = tea.Position
 
-func main() {
-
-	// Load environment variables from .env file
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	log.Println("Connecting to the 'fenjan' database 🐰.")
-	db, err := sql.Open("mysql", getDbConnectionString())
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println("Creating the 'helsinki_fi' table in the 'fenjan' database if not exists 👾.")
-	createTableIfNotExists(db)
-
-	// Get the URLs from the database
-	visitedUrls := getUrlsFromDB(db)
-
-	log.Println("Searching the University of Helsinki  for the Ph.D. vacancies 🦉.")
-	positions, err := getAndParseData()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// Loop through each position
-	newPositions := []Position{}
-	for _, position := range positions {
-
-		// Check if the URL has been visited before
-		if visitedUrls[position.URL] {
-			log.Println("URL has been visited before:", position.URL)
-			continue
-		}
-		newPositions = append(newPositions, position)
-	}
-	log.Println("Extracted details of", len(newPositions), "open positions 🤓.")
-
-	log.Println("Saving new positions to the database 🚀...")
-	savePositionsToDB(db, newPositions)
-	log.Println("Finished 🫡!")
-
-}
-
-func getAndParseData() ([]Position, error) {
+func getPositions() ([]Position, error) {
 	// Get request
 	resp, err := http.Get("https://www.helsinki.fi/en/ajax_get_jobs/en/null/null/null/0")
 	if err != nil {
@@ -90,77 +35,65 @@ func getAndParseData() ([]Position, error) {
 	if err := json.Unmarshal(body, &result); err != nil { // Parse []byte to the go struct pointer
 		return nil, err
 	}
-
-	var positions []Position
-	if err := json.Unmarshal([]byte(result[0].Data), &positions); err != nil { // Parse []byte to the go struct pointer
+	var positionsAjax []struct {
+		Title         string `json:"title"`
+		URL           string `json:"url"`
+		Department    string `json:"department"`
+		Date          string `json:"date"`
+		DateTimestamp int    `json:"date_timestamp"`
+		Description   string `json:"description"`
+		Key           int    `json:"key"`
+	}
+	if err := json.Unmarshal([]byte(result[0].Data), &positionsAjax); err != nil { // Parse []byte to the go struct pointer
 		return nil, err
+	}
+
+	positions := []Position{}
+
+	for _, position := range positionsAjax {
+		positions = append(positions, Position{position.Title, position.URL, position.Description, position.Date})
 	}
 	return positions, nil
 }
 
-// getDbConnectionString function reads the database connection details from environment variables
-func getDbConnectionString() string {
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	username := os.Getenv("DB_USERNAME")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
-	return username + ":" + password + "@tcp(" + host + ":" + port + ")/" + dbname
-}
+func main() {
 
-// createTableIfNotExists function creates the "helsinki_fi" table if it doesn't already exist
-func createTableIfNotExists(db *sql.DB) {
-	// SQL statement to create the table
-	query := `CREATE TABLE IF NOT EXISTS helsinki_fi (
-		id INT AUTO_INCREMENT PRIMARY KEY,
-		title VARCHAR(255) NOT NULL,
-		description TEXT NOT NULL,
-		url VARCHAR(255) NOT NULL,
-		date VARCHAR(255) NOT NULL
-	)`
-
-	// Execute the SQL statement
-	_, err := db.Exec(query)
+	// Define name of the table for the University of Helsinki positions
+	tableName := "helsinki_fi"
+	log.Println("Connecting to the 'fenjan' database 🐰.")
+	db, err := sql.Open("mysql", tea.GetDbConnectionString())
 	if err != nil {
 		log.Fatal(err)
 	}
-}
 
-// savePositionsToDB function saves the scraped positions to a MySQL database
-func savePositionsToDB(db *sql.DB, positions []Position) {
-	// Prepare the SQL statement
-	stmt, err := db.Prepare("INSERT INTO helsinki_fi (title, description, url, date) VALUES (?, ?, ?, ?)")
+	log.Println("Creating to the " + tableName + " table in the 'fenjan' database if not exists 👾.")
+	tea.CreateTableIfNotExists(db, tableName)
+
+	// Get the URLs from the database
+	visitedUrls := tea.GetUrlsFromDB(db, tableName)
+
+	log.Println("Searching the University of Helsinki  for the Ph.D. vacancies 🦉.")
+	positions, err := getPositions()
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		return
 	}
-	defer stmt.Close()
 
-	// Loop through each position and execute the SQL statement
+	// Loop through each position
+	newPositions := []Position{}
 	for _, position := range positions {
-		_, err := stmt.Exec(position.Title, position.Description, position.URL, position.Date)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-}
 
-// getUrlsFromDB function return a map of all urls in the "positions" table
-func getUrlsFromDB(db *sql.DB) map[string]bool {
-	// SELECT statement to retrieve URLs from the table
-	rows, err := db.Query("SELECT url FROM helsinki_fi")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-
-	// Create a map to store the URLs
-	urls := make(map[string]bool)
-	for rows.Next() {
-		var url string
-		if err := rows.Scan(&url); err != nil {
-			log.Fatal(err)
+		// Check if the URL has been visited before
+		if visitedUrls[position.URL] {
+			log.Println("URL has been visited before:", position.URL)
+			continue
 		}
-		urls[url] = true
+		newPositions = append(newPositions, position)
 	}
-	return urls
+	log.Println("Extracted details of", len(newPositions), "open positions 🤓.")
+
+	log.Println("Saving new positions to the database 🚀...")
+	tea.SavePositionsToDB(db, newPositions, tableName)
+	log.Println("Finished 🫡!")
+
 }
