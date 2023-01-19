f2f08a579047ab994a15643e950591093411df8a
mh-salari <mh.salari@sutech.ac.ir>
2023-01-19 18:54:35 +0330

    add: crawler for Freie Universität Berlin

---
+++ go_crawlers/freie_universitat_berlin/go.mod
@@ -0,0 +1,35 @@
+module fenjan.ai-hue.ir/freie_universitat_berlin
+
+replace fenjan.ai-hue.ir/tea => ../tea
+
+go 1.18
+
+require (
+	fenjan.ai-hue.ir/tea v0.0.0-00010101000000-000000000000
+	github.com/gocolly/colly v1.2.0
+	github.com/mmcdole/gofeed v1.1.3
+
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/antchfx/htmlquery v1.2.6 // indirect
+	github.com/antchfx/xmlquery v1.3.14 // indirect
+	github.com/antchfx/xpath v1.2.2 // indirect
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.3.1 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/mmcdole/goxpp v0.0.0-20181012175147-0068e33feabf // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
+	golang.org/x/net v0.5.0 // indirect
+	golang.org/x/text v0.6.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+)
---
+++ go_crawlers/freie_universitat_berlin/main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"fenjan.ai-hue.ir/tea"
+	"github.com/gocolly/colly"
+	"github.com/mmcdole/gofeed"
+)
+
+type Position = tea.Position
+
+func getPositionsUrlsFromRSS() (urls []string) {
+	fp := gofeed.NewParser()
+	feed, err := fp.ParseURL("https://www.fu-berlin.de/universitaet/beruf-karriere/jobs/english/index.rss")
+	if err != nil {
+		log.Panicln("Error in parsing the RSS file", err)
+	}
+
+	for _, item := range feed.Items {
+		urls = append(urls, item.Link)
+	}
+	return urls
+}
+
+func getPositionDescription(url string) Position {
+	var description string
+	var title string
+	var date string
+	c := colly.NewCollector()
+	c.SetRequestTimeout(60 * time.Second)
+
+	c.OnHTML("div.box-job-offer-header h2", func(e *colly.HTMLElement) {
+		title = strings.TrimSpace(e.Text)
+
+	})
+
+	c.OnHTML("h3", func(e *colly.HTMLElement) {
+
+		if strings.Contains(e.Text, "Bewerbungsende") {
+
+			date = strings.TrimSpace(strings.ReplaceAll(e.Text, "Bewerbungsende: ", ""))
+		}
+	})
+
+	c.OnHTML("div.editor-content p", func(e *colly.HTMLElement) {
+		description += strings.TrimSpace(e.Text)
+
+	})
+
+	// Set error handler
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Request failed ☠️!", "Error:", err)
+		r.Request.Retry()
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL, "🦍")
+	})
+
+	c.Visit(url)
+
+	return Position{Title: title, URL: url, Description: description, Date: date}
+
+}
+
+func main() {
+
+	// Define name of the table for the Freie Universität Berlin
+	tableName := "fu_berlin_de"
+	log.Println("Connecting to the 'fenjan' database 🐰.")
+	db, err := sql.Open("mysql", tea.GetDbConnectionString())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Creating to the " + tableName + " table in the 'fenjan' database if not exists 👾.")
+	tea.CreateTableIfNotExists(db, tableName)
+
+	// Get the URLs from the database
+	visitedUrls := tea.GetUrlsFromDB(db, tableName)
+
+	log.Println("Finding URLs of open positions in Freie Universität Berlin 🦉.")
+	positionsUrl := getPositionsUrlsFromRSS()
+	log.Printf("Found %d open positions", len(positionsUrl))
+
+	// Loop through each position
+	positions := []Position{}
+	for _, url := range positionsUrl[:] {
+
+		// Check if the URL has been visited before
+		if visitedUrls[url] {
+			log.Println("URL has been visited before:", url)
+			continue
+		}
+		position := getPositionDescription(url)
+		positions = append(positions, position)
+	}
+	log.Println("Extracted details of", len(positions), "open positions 🤓.")
+
+	log.Println("Saving new positions to the database 🚀...")
+	tea.SavePositionsToDB(db, positions, tableName)
+	log.Println("Finished 🫡!")
+
+}
