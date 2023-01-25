3c5f4da2d5b958ce1ea9e11549a73c22e44748df
mh-salari <mh.salari@sutech.ac.ir>
2023-01-25 06:15:19 +0330

    add: a template project for golang webs craping codes

---
+++ go_crawlers/_template/_go.mod
@@ -0,0 +1,7 @@
+module fenjan.ai-hue.ir/__UNI_NAME__
+
+replace fenjan.ai-hue.ir/tea => ../utils/tea
+
+replace fenjan.ai-hue.ir/logger => ../utils/logger
+
+go 1.18
---
+++ go_crawlers/_template/_main.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"math/rand"
+	"time"
+
+	"fenjan.ai-hue.ir/logger"
+	"fenjan.ai-hue.ir/tea"
+	"github.com/gocolly/colly"
+)
+
+// Set the university name, the database table name for this university, and the url of vacant positions
+var uniName string = ""
+var tableName string = ""
+var vacantPositionsUrl string = ""
+
+// Get Position type from tea helper package
+type Position = tea.Position
+
+// get the URL of all vacant positions
+func getPositionsUrls() (urls []string) {
+
+	c := colly.NewCollector()
+	c.SetRequestTimeout(60 * time.Second)
+
+	// 🧟‍♀️------------------------------------------------------------------------------
+
+	// Add the OnRequest function to log the URLs that are visited
+	c.OnRequest(func(r *colly.Request) {
+		log.Println("Visiting", r.URL, "🥷")
+	})
+
+	// Set error handler
+	c.OnError(func(r *colly.Response, err error) {
+		log.Println("Request failed ☠️!", "Error:", err)
+
+		// Sleep if its a 429 Too Many Requests Error
+		if r.StatusCode == 429 {
+			rand.Seed(time.Now().UnixNano())
+			n := 30 + rand.Intn(60)
+			log.Printf("Sleeping %d seconds...\n", n)
+			time.Sleep(time.Duration(n) * time.Second)
+		}
+
+		// Retry for 5 time
+		retriesLeft := tea.RetryRequest(r, 5)
+		if retriesLeft == 0 {
+			logger.Error.Fatal("Reached max number of retries 🫄! ", "Error: ", err)
+		}
+	})
+
+	c.Visit(vacantPositionsUrl)
+
+	return urls
+}
+
+// Get the details of position
+func getPositionDescription(url string) (position Position) {
+
+	c := colly.NewCollector()
+	c.SetRequestTimeout(60 * time.Second)
+
+	// 🧟‍♀️------------------------------------------------------------------------------
+
+	// Add the OnRequest function to log the URLs that are visited
+	c.OnRequest(func(r *colly.Request) {
+		log.Println("Visiting", r.URL, "🥷")
+	})
+
+	// Set error handler
+	c.OnError(func(r *colly.Response, err error) {
+		log.Println("Request failed ☠️!", "Error:", err, "Retrying 🧌!")
+
+		// Sleep if its a 429 Too Many Requests Error
+		if r.StatusCode == 429 {
+			rand.Seed(time.Now().UnixNano())
+			n := 30 + rand.Intn(60)
+			log.Printf("Sleeping %d seconds...\n", n)
+			time.Sleep(time.Duration(n) * time.Second)
+		}
+
+		// Retry for 5 time
+		retriesLeft := tea.RetryRequest(r, 5)
+		if retriesLeft == 0 {
+			logger.Error.Fatal("Reached max number of retries 🫄! ", "Error: ", err)
+		}
+	})
+
+	c.Visit(url)
+
+	position.URL = url
+
+	return position
+
+}
+
+func main() {
+
+	// Connecting to the database and creating the university table if not exist
+	log.Println("Connecting to the 'fenjan' database 🐰.")
+	db, err := sql.Open("mysql", tea.GetDbConnectionString())
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Creating the '%s' table in the 'fenjan' database if not exists 👾.", tableName)
+	tea.CreateTableIfNotExists(db, tableName)
+
+	// Get the URLs from the database
+	visitedUrls := tea.GetUrlsFromDB(db, tableName)
+
+	// Getting the URL of vacant positions on the university site
+	log.Printf("Searching the %s for the Ph.D. vacancies 🦉.", uniName)
+	positions := []Position{}
+	positionsUrls := getPositionsUrls()
+	log.Println("Found ", len(positionsUrls), " open positions 🐝")
+
+	// Extract description of the positions
+	for _, url := range positionsUrls {
+		// Check if the URL has been visited before
+		if visitedUrls[url] {
+			log.Println("URL has been visited before:", url)
+			continue
+		}
+		position := getPositionDescription(url)
+		positions = append(positions, position)
+	}
+	log.Println("Extracted details of", len(positions), "open positions 🤓.")
+
+	// Saving the positions to the database
+	log.Println("Saving new positions to the database 🚀...")
+	tea.SavePositionsToDB(db, positions, tableName)
+
+	log.Println("Finished 🫡!")
+
+}
